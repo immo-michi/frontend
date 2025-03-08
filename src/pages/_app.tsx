80ebfcf54c7ad41495debe34da7a1235ea19e2f5
Michael Schramm <michael@tucan.ai>
2025-03-09 00:25:19 +0100

    update build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,30 +8,35 @@ import React from 'react'
 import { wrapper } from 'src/store'
 import '../css/globals.scss'
 import getApolloClient from '../graphql/client'
+import { Provider as ReduxProvider } from 'react-redux'
 import { NextConfigType } from '../../next.config.type'
 
 const config = getConfig() as NextConfigType
 
-const App: AppType = ({ Component, pageProps, ...other }) => {
+const App: AppType = ({ Component, ...pageProps }) => {
+  const { store, props } = wrapper.useWrappedStore(pageProps)
+
   return (
-    <ApolloProvider client={getApolloClient(config)}>
-      <AntdApp>
-      <Head>
-        <title>Immo Michi</title>
-        <meta
-          name={'viewport'}
-          content={'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'}
-          key={'viewport'}
-        />
-        <link key={'favicon'} rel={'shortcut icon'} href={'/favicon.png'} type={'image/png'} />
-      </Head>
-      <AppSuspense>
-        {/* Workaround for https://github.com/vercel/next.js/issues/8592 */}
-        <Component {...pageProps} err={(other as any).err} />
-      </AppSuspense>
-      </AntdApp>
-    </ApolloProvider>
+    <ReduxProvider store={store}>
+      <ApolloProvider client={getApolloClient(config)}>
+        <AntdApp>
+        <Head>
+          <title>Immo Michi</title>
+          <meta
+            name={'viewport'}
+            content={'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'}
+            key={'viewport'}
+          />
+          <link key={'favicon'} rel={'shortcut icon'} href={'/favicon.png'} type={'image/png'} />
+        </Head>
+        <AppSuspense>
+          {/* Workaround for https://github.com/vercel/next.js/issues/8592 */}
+          <Component {...pageProps} err={(props as any).err} />
+        </AppSuspense>
+        </AntdApp>
+      </ApolloProvider>
+    </ReduxProvider>
   )
 }
 
-export default wrapper.withRedux(App)
+export default App
